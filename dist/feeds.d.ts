0a6dcbbd6562c25340808fc85564ab8a3d5d63e9
Simon Ganiere <simon.ganiere@gmail.com>
2025-08-06 21:17:21 +0200

    .gitignore is now working

--- dist/feeds.d.ts
+++
@@ -1,6 +0,0 @@
-import { CyberSecurityFeed } from './types.js';
-export declare const CYBERSECURITY_FEEDS: CyberSecurityFeed[];
-export declare function getFeedByName(name: string): CyberSecurityFeed | undefined;
-export declare function getFeedsByCategory(category: CyberSecurityFeed['category']): CyberSecurityFeed[];
-export declare function getAllFeedUrls(): string[];
-//# sourceMappingURL=feeds.d.ts.map
